7ce7e862aa2e37ddeb741a456d72fcb6f120cfe9
zhuoqianmingyue <ljk126wy@126.com>
2020-07-25 14:36:51 +0800

    自定义彩色banner 日志输出

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -1,12 +1,22 @@
 package com.company.project;
 
+import com.company.project.core.SystemLogColorConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author：zhuoqianmingyue
+ * @Date： 2020/07/11
+ * @Description：自定义符号日志输出代码来源 RuoYi：https://github.com/lerry903/RuoYi（在其基础上为其添加颜色输出展示）
+ **/
 @SpringBootApplication
 public class Application {
+    private static Logger log = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info(SystemLogColorConstant.YELLOW +"(♥◠ ‿ ◠) ﾉﾞ"+ SystemLogColorConstant.END+"：MonKey 启动成功 没有发现异常"+" "+ SystemLogColorConstant.YELLOW+   "ლ ( ´ڡ` ლ) ﾞ"+ SystemLogColorConstant.END+" "+ SystemLogColorConstant.BULLUE +  " OH YEAH ！"+ SystemLogColorConstant.END );
     }
 }
 
---
+++ src/main/java/com/company/project/core/SystemLogColorConstant.java
@@ -0,0 +1,18 @@
+package com.company.project.core;
+
+/**
+ * 系统日志彩色日志参数数据字典类
+ *
+ * @Author： zhuoqianmingyue
+ * @Date： 2020/6/25 10:46 上午
+ * @Description：
+ **/
+public class SystemLogColorConstant {
+    private SystemLogColorConstant(){throw new IllegalStateException("Utility class");}
+    /**黄颜色*/
+    public static final String YELLOW = "\033[33;3m";
+    /**绿颜色*/
+    public static final String BULLUE = "\033[32;2m";
+    /**颜色结束标志*/
+    public static final String END = "\033[0m";
+}
