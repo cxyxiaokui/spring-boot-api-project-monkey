562282621248ec518037d177f5c5616798b6ab77
zhuoqianmingyue <ljk126wy@126.com>
2020-08-09 09:54:18 +0800

    修改demo-user.sql

--- src/test/resources/demo-user.sql
+++ src/test/resources/demo-user.sql
@@ -1,45 +1,47 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat MySQL Data Transfer
 
-Source Server         : Localhost
-Source Server Version : 50713
-Source Host           : localhost:3306
-Source Database       : test
+ Source Server         : 本地数据库
+ Source Server Type    : MySQL
+ Source Server Version : 50648
+ Source Host           : localhost
+ Source Database       : monkey
 
-Target Server Type    : MYSQL
-Target Server Version : 50713
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50648
+ File Encoding         : utf-8
 
-Date: 2017-06-23 14:25:27
+ Date: 08/09/2020 09:48:05 AM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for user
+--  Table structure for `demo_user`
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `demo_user`;
+CREATE TABLE `demo_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nick_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `sex` int(1) DEFAULT NULL,
-  `register_date` datetime NOT NULL,
+  `register_date` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_by` bigint(20) DEFAULT NULL,
+  `create_by` bigint(20) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `delete_flag` tinyint(2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=208 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of user
+--  Records of `demo_user`
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '89921218@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('2', '2@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-2', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('3', '3@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-3', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('4', '4@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-4', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('5', '5@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-5', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('6', '6@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-6', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('7', '7@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-7', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('8', '8@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-8', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('9', '9@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-9', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('10', '10@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-10', '1', '2017-06-23 14:24:23');
-SET FOREIGN_KEY_CHECKS=1;
+BEGIN;
+INSERT INTO `demo_user` VALUES ('46', 'Tt0ekpw8c90ld', 'Wvuio0jph', 'xiaoliuzhuoqianmingyuexiaolixiaoliuzhuoqianmingyuexiaoli', '3y2d_zcehb14d@JOBq.ape', '0', '2018-11-23 00:00:00', '2020-08-08 10:18:53', null, null, null, '1');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
